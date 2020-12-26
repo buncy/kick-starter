4b0a75a79c79e602b14608830eca9317c7cc1fa2
karthik bokkesam <karthik138105@gmail.com>
2020-12-26 23:32:58 +0530

    first commit

---
+++ src/campaign.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+
+contract kickStartCampaign{
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    
+    modifier restricted(){
+        require(msg.sender==manager);
+        _;
+    }
+    
+    function campaign(uint minimum)public{
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    function contribute () public payable {
+        require(msg.value>minimumContribution);
+        approvers.push(msg.sender);
+    }
+    function createRequest(string description, uint value,address recipient)
+    public restricted{
+        Request memory newRequest = Request({
+            description:description,
+            value:value,
+            recipient:recipient,
+            complete:false
+        });
+        requests.push(newRequest);
+    }
+}
