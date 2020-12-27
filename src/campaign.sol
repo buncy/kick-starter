7ec3c15b0d8ecf6eef1d09149c74bb6278461a9d
karthik bokkesam <karthik138105@gmail.com>
2020-12-27 20:01:28 +0530

    create factory

--- src/campaign.sol
+++ src/campaign.sol
@@ -1,38 +1,77 @@
 pragma solidity ^0.4.17;
 
-contract kickStartCampaign{
-    struct Request{
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    constructor(uint256 minimum) public {
+        address newCampaign = new KickStartCampaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract KickStartCampaign {
+    struct Request {
         string description;
-        uint value;
+        uint256 value;
         address recipient;
         bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
     }
     Request[] public requests;
     address public manager;
-    uint public minimumContribution;
-    address[] public approvers;
-    
-    modifier restricted(){
-        require(msg.sender==manager);
+    uint256 public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint256 public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
         _;
     }
-    
-    function campaign(uint minimum)public{
-        manager = msg.sender;
+
+    constructor(uint256 minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
-    function contribute () public payable {
-        require(msg.value>minimumContribution);
-        approvers.push(msg.sender);
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
     }
-    function createRequest(string description, uint value,address recipient)
-    public restricted{
-        Request memory newRequest = Request({
-            description:description,
-            value:value,
-            recipient:recipient,
-            complete:false
-        });
+
+    function createRequest(
+        string description,
+        uint256 value,
+        address recipient
+    ) public restricted {
+        Request memory newRequest =
+            Request({
+                description: description,
+                value: value,
+                recipient: recipient,
+                complete: false,
+                approvalCount: 0
+            });
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public restricted {
+        Request storage request = requests[index];
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
