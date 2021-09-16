e45bd731be441afc5a44f3b32e73fe4c9b1d79f6
buncy <karthik138105@gmail.com>
2021-09-16 09:05:56 +0530

    init frontend

--- ethereum/contracts/campaign.sol
+++ ethereum/contracts/campaign.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.17;
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    constructor(uint256 minimum) public {
-        address newCampaign = new KickStartCampaign(minimum, msg.sender);
+    function createCampaign(uint256 minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
 
@@ -13,7 +13,7 @@ contract CampaignFactory {
     }
 }
 
-contract KickStartCampaign {
+contract Campaign {
     struct Request {
         string description;
         uint256 value;
@@ -22,6 +22,7 @@ contract KickStartCampaign {
         uint256 approvalCount;
         mapping(address => bool) approvals;
     }
+
     Request[] public requests;
     address public manager;
     uint256 public minimumContribution;
@@ -33,14 +34,16 @@ contract KickStartCampaign {
         _;
     }
 
-    constructor(uint256 minimum, address creator) public {
+    function Campaign(uint256 minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
+
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(
@@ -48,29 +51,33 @@ contract KickStartCampaign {
         uint256 value,
         address recipient
     ) public restricted {
-        Request memory newRequest =
-            Request({
-                description: description,
-                value: value,
-                recipient: recipient,
-                complete: false,
-                approvalCount: 0
-            });
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
         requests.push(newRequest);
     }
 
     function approveRequest(uint256 index) public {
         Request storage request = requests[index];
+
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
+
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
 
     function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
+
         require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
+
         request.recipient.transfer(request.value);
         request.complete = true;
     }
